2c796c7da19b72152c922ce9e631dea8cc8bac50
Larissa Ferme <ferme@sipgate.de>
2024-12-01 21:24:16 +0100

    add Alertmanager and error route

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # prometheusExample
 
-Grafana runs on http://localhost:3000
+Grafana runs on http://localhost:3000  `sudo systemctl status grafana-server`
 
 Prometheus runs on http://localhost:9090 and can be started by running `./prometheus --config.file=prometheus.yml`
 
 Spring-application runs on http://localhost:8080
+
+Alertmanager runs on http://localhost:9093 and can be started by running `./alertmanager --config.file=alertmanager.yml`
--- src/main/java/com/ferme/learning/prometheus_ha/controller/Controller.java
+++ src/main/java/com/ferme/learning/prometheus_ha/controller/Controller.java
@@ -1,9 +1,12 @@
 package com.ferme.learning.prometheus_ha.controller;
 
+import io.micrometer.core.annotation.Timed;
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,16 +14,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     private final Counter counter;
+    private final Counter errorCounter;
 
     public Controller(MeterRegistry registry){
         this.counter = Counter.builder("api.hello.calls")
-                                .description("Total number of hello enpoint calls")
+                                .description("Total number of hello endpoint calls")
+                .               register(registry);
+        this.errorCounter = Counter.builder("api.error_requests.calls")
+                .description("Total number of error requests endpoint calls")
+                .tag("status", "500")
                 .               register(registry);
     }
 
+    @Timed(value = "api.get_requests", description = "GET request response time")
     @GetMapping("/hello")
     public String hello() {
         counter.increment();
         return "Das ist der Prometheus-Test";
+    }
+
+    @Timed(value = "api.error_requests", description = "Error request response time")
+    @GetMapping("/error")
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String error() {
+        errorCounter.increment();
+        throw new RuntimeException("Simulierter Serverfehler");
     }
 }
